7a77a2856d3e503f1228c6c14132569f3b4b7122
Noga geller <nogag098@gmail.com>
2024-07-02 14:22:26 +0300

    new

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid';
-import Task from './Task';
 import Typography from '@mui/material/Typography';
 //import Button from '@mui/material/Button'
 import AddTaskDialog from './AddTaskDialog';
