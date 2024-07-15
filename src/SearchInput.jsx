bc3cdd3d95fac1544e766071faf6c09bba61affc
Noga geller <nogag098@gmail.com>
2024-07-15 11:17:11 +0300

    filter without debaunce

--- src/SearchInput.jsx
+++ src/SearchInput.jsx
@@ -1,38 +1,80 @@
 import { TextField } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { pink } from '@mui/material/colors';
+import UseFilterTodos from './hooks/UseFilterTodos';
+import UseTodos from './hooks/UseTodos';
 
 const SearchInput = () => {
-  return (
-    <div>
-          <TextField
-              autoFocus
-              margin="dense"
-              id="searchTask"
-              label="Search tasks..."
-              variant="outlined"
-              type="text"
-              name='searchTask'
-              sx={{
-                  bgcolor: 'white',
-                  borderRadius: '20px',
-                  '& .MuiInputLabel-root': {
-                      color: pink[600],
-                  },
-                  '& .MuiOutlinedInput-root': {
-                      '&.Mui-focused fieldset': {
-                          borderColor: 'white',
-                      },
-                  },
-                  '& .MuiFormLabel-root.Mui-focused': {
-                      color: pink[600],
-                  },
-              }}
-              //value={formData.name || ''}
-              //onChange={handleChange}
-          />
-    </div>
-  )
+
+    const [searchInput, setSearchInput] = useState('')
+    const { setFilterTasks } = UseFilterTodos();
+    const { tasks } = UseTodos();
+
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setSearchInput(value);
+        if (value) {
+            filterTasksBySearch();
+        }
+        else {
+            const updatedTasks = tasks.filter(task => task);
+            setFilterTasks(updatedTasks);
+        }
+    };
+
+    const filterTasksBySearch = () => {
+        const updatedTasks = tasks.filter(task => task.name.includes(searchInput));
+        setFilterTasks(updatedTasks);
+    }
+
+    // useEffect(() => {
+    //     const handler = setTimeout(() => {
+    //         console.log(searchInput);
+    //         if (searchInput) {
+    //             const updatedTasks = tasks.filter(task => task.name.includes(searchInput));
+    //             setFilterTasks(updatedTasks);
+    //         }
+    //         else {
+    //             const updatedTasks = tasks.filter(task => task);
+    //             setFilterTasks(updatedTasks);
+    //         }
+    //     }, 300);
+
+    //     return () => {
+    //         clearTimeout(handler);
+    //     };
+    // }, [searchInput]);
+
+    return (
+        <div>
+            <TextField
+                autoFocus
+                margin="dense"
+                id="searchTask"
+                label="Search tasks..."
+                variant="outlined"
+                type="text"
+                name='searchTask'
+                sx={{
+                    bgcolor: 'white',
+                    borderRadius: '20px',
+                    '& .MuiInputLabel-root': {
+                        color: pink[600],
+                    },
+                    '& .MuiOutlinedInput-root': {
+                        '&.Mui-focused fieldset': {
+                            borderColor: 'white',
+                        },
+                    },
+                    '& .MuiFormLabel-root.Mui-focused': {
+                        color: pink[600],
+                    },
+                }}
+                value={searchInput || ''}
+                onChange={handleChange}
+            />
+        </div>
+    )
 }
 
 export default SearchInput
