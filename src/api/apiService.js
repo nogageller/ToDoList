3414071101287b68956ee640fd6a0979f840d2b1
Noga geller <nogag098@gmail.com>
2024-09-04 17:08:41 +0300

    remove try and catch

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,73 +1,38 @@
 import axios from '../api/axios';
 
 export const getTasks = async (filter = '') => {
-    try {
-        const response = await axios.get('/tasks', {
-            params: { filter }
-        });
-        console.log('Fetched tasks:', response.data);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching tasks:', error);
-        throw error;
-    }
+    const response = await axios.get('/tasks', {
+        params: { filter }
+    });
+    console.log('Fetched tasks:', response.data);
+    return response.data;
 };
 
 export const getSubjects = async () => {
-    try {
-        const response = await axios.get('/subjects');
-        console.log('Fetched subjects:', response.data);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching tasks:', error);
-        throw error;
-    }
+    const response = await axios.get('/subjects');
+    console.log('Fetched subjects:', response.data);
+    return response.data;
 };
 
 export const createTask = async (task) => {
-    try {
-        const response = await axios.post('/tasks', task);
-        return response.data;
-    } catch (error) {
-        console.error('Error creating task:', error);
-        throw error;
-    }
+    const response = await axios.post('/tasks', task);
+    return response.data;
 };
 
 export const updateTask = async ({ id, updatedTask }) => {
-    try {
-        const response = await axios.put(`/tasks/${id}`, updatedTask);
-        return response.data;
-    } catch (error) {
-        console.error('Error updating task:', error);
-        throw error;
-    }
+    const response = await axios.put(`/tasks/${id}`, updatedTask);
+    return response.data;
 };
 
 export const updateTaskCompletion = async (task) => {
-    try {
-        const response = await axios.patch(`/tasks/${task._id}`, { isChecked: !task.isChecked });
-        return response.data;
-    } catch (error) {
-        console.error('Error updating task:', error);
-        throw error;
-    }
+    const response = await axios.patch(`/tasks/${task._id}`, { isChecked: !task.isChecked });
+    return response.data;
 };
 
 export const deleteTask = async (id) => {
-    try {
-        await axios.delete(`/tasks/${id}`);
-    } catch (error) {
-        console.error('Error deleting task:', error);
-        throw error;
-    }
+    await axios.delete(`/tasks/${id}`);
 };
 
 export const deleteDoneTasks = async () => {
-    try {
-        await axios.delete(`/tasks/deleteDone`);
-    } catch (error) {
-        console.error('Error deleting task:', error);
-        throw error;
-    }
+    await axios.delete(`/tasks/deleteDone`);
 };
